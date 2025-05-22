4215f3996af8b6224a122186f1ed2cbf81d2b11e
Jean GIESE <jean.giese@etu.unistra.fr>
2025-05-22 16:30:10 +0200

    Ajout des premières structures

--- reseau.h
+++ reseau.h
@@ -1,8 +1,32 @@
 typedef uint_8 MAC;
 typedef uint_8 IP;
 
-struct station {
+typedef struct Station {
     MAC adrMAC;
     IP adrIP;
     char nom[32];
-}
+} Station;
+
+typedef struct Hub {
+    char nom[32]
+    size_t nb_ports;
+} Hub;
+
+typedef struct Trame {
+    MAC destination;
+    MAC source;
+    char data[1500];
+} Trame;
+
+typedef struct Commutation {
+    MAC adrMAC;
+    uint_32 port;
+} Commutation;
+    
+typedef struct wafenSSwitch {
+    char nom[32];
+    MAC adrMAC;
+    size_t nb_ports;
+    uint_16 priorite;
+    Commutation tabCommutation[nb_ports];
+} wafenSSwitch;
