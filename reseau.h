c0166b4ff2b4b57ea7360ade61a3a004865a6c69
Hugo Leger <legerh@unistra.fr>
2025-06-02 11:02:02 +0200

    update de la week

--- reseau.h
+++ reseau.h
@@ -1,40 +1,52 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef uint8_t MAC;
 typedef uint8_t IP;
 
 typedef struct Station {
-    MAC adrMAC;
-    IP adrIP;
+    MAC adrMAC[6];
+    IP adrIP[4];
     char nom[32];
 } Station;
 
 typedef struct Hub {
-    char nom[32]
+    char nom[32];
     size_t nb_ports;
 } Hub;
 
 typedef struct Trame {
-    MAC destination;
-    MAC source;
+    MAC destination[6];
+    MAC source[6];
     char data[1500];
 } Trame;
 
 typedef struct Commutation {
-    MAC adrMAC;
+    MAC adrMAC[6];
     uint32_t port;
 } Commutation;
-    
+
 typedef struct Switch {
     char nom[32];
-    MAC adrMAC;
+    MAC adrMAC[6];
     size_t nb_ports;
     uint16_t priorite;
     Commutation *tabCommutation;
 } Switch;
 
+typedef enum {
+    TYPE_HUB,
+    TYPE_SWITCH,
+    TYPE_STATION
+} TypeObjet;
+
 typedef struct Sommet {
-    
+    TypeObjet type;
+    union {
+        Station station;
+        Switch sw;
+        Hub hub;
+    } objet;
 } Sommet;
